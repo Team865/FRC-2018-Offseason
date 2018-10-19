48abe2e81a7778b32dd8fe10c1d100aca9b072d6
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-10-19 11:59:50 -0400

    add FMS to OneSwitch

--- frc2018_3/src/main/java/ca/warp7/frc2018_3/auto/modes/OneSwitch.java
+++ frc2018_3/src/main/java/ca/warp7/frc2018_3/auto/modes/OneSwitch.java
@@ -6,14 +6,26 @@ import ca.warp7.frc.commons.scheduler.IAction;
 import ca.warp7.frc.commons.state.PIDValues;
 import ca.warp7.frc2018_3.auto.actions.DriveForDistanceAction;
 import ca.warp7.frc2018_3.auto.actions.OuttakeCube;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class OneSwitch implements IAutoMode {
+
+    private char side;
+
+    public OneSwitch(char side) {
+        this.side = side;
+    }
+
     @Override
     public IAction getMainAction() {
-        return new CompositeAction()
-                .startSeries()
-                .add(new DriveForDistanceAction(new PIDValues(0.018, 0.00001, 0.23), 103, 5))
-                .add(new OuttakeCube(0.5, -0.75))
-                .getActionGraph();
+        String gameMessage = DriverStation.getInstance().getGameSpecificMessage();
+        if (gameMessage.charAt(0) == side) {
+            return new CompositeAction()
+                    .startSeries()
+                    .add(new DriveForDistanceAction(new PIDValues(0.018, 0.00001, 0.23), 103, 5))
+                    .add(new OuttakeCube(0.5, -0.75))
+                    .getActionGraph();
+        }
+        return new DriveForDistanceAction(new PIDValues(0.018, 0.00001, 0.23), 103, 5);
     }
 }
